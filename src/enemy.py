cd7792921d4d396c55c4dd04813fcabc79d5e8a6
Chloe-Zou <142557462+Chloe-Zou@users.noreply.github.com>
2024-05-08 12:07:33 -0600

    Update enemy.py

--- src/enemy.py
+++ src/enemy.py
@@ -1,1 +1,16 @@
+import pygame
 
+class character:
+  def __init__(self, stage, speed, power, health, stamina):
+    self.stage = stage
+    self.speed = speed
+    self.power = power
+    self.health = health
+    self.stamina = stamina
+
+  def enemyStrength(self, stage):
+    return stage
+  def speciality(self, power):
+    return False
+  def cook(self, stamina, health):
+    return health, stamina
