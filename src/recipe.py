b98246f118bfbba637f2903655abd99b3402c02d
Chloe-Zou <142557462+Chloe-Zou@users.noreply.github.com>
2024-05-10 12:18:59 -0600

    Update recipe.py

--- src/recipe.py
+++ src/recipe.py
@@ -1,4 +1,11 @@
-ingredients = ['onion', 'potato', 'garlic', 'tomato', 'salt', 'pepper', 'chicken', 
-  'lettuce', 'bread', 'ham']
+import random
 
-# mix():
+chop = ['onion', 'potato', 'garlic', 'tomato']
+
+other= ['salt', 'pepper', 'chicken', 'lettuce', 'bread', 'ham']
+
+def mix(num):
+  if num == 2:
+    ing1 = random.randint(0,9)
+    ing2 = random.randint(0,9)
+    print("Recipe: " + chop[ing1]+ " " +other[ing2])
