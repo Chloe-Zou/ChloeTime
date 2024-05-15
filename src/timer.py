89a17e69720454b668b314edcadaa0385d6883b8
Chloe-Zou <142557462+Chloe-Zou@users.noreply.github.com>
2024-05-15 12:13:37 -0600

    Update timer.py

--- src/timer.py
+++ src/timer.py
@@ -2,6 +2,16 @@ import pygame
 import pygame.freetype
 from pygame.locals import K_1, KEYDOWN, K_2
 
+global out
+
+def start():
+  global out
+  start_time = 0
+  ticks = pygame.time.get_ticks() - start_time
+  millis = ticks % 1000
+  seconds = int(ticks / 1000 % 60)
+  minutes = int(ticks / 60000 % 24)
+  out = '{minutes:02d}:{seconds:02d}:{millis}'.format(minutes=minutes, millis=millis, seconds=seconds)
 
 def result():
   pygame.init()
@@ -12,27 +22,12 @@ def result():
   while font.origin:
     font.render_to(screen, (100, 100),  out, pygame.Color('dodgerblue'))
     pygame.display.flip()
-    
-def renderClock(screen):
-  font.origin = True
-  screen.fill(pygame.Color('grey12'))
-  ticks = pygame.time.get_ticks()
-  millis = ticks % 1000
-  seconds = int(ticks / 1000 % 60)
-  minutes = int(ticks / 60000 % 24)
-  #formatting numbers
-  out = '{minutes:02d}:{seconds:02d}:{millis}'.format(minutes=minutes, millis=millis, seconds=seconds)
-  font.render_to(screen, (100,100), out, pygame.Color('dodgerblue'))
-  pygame.display.flip()
-  clock.tick(60)
 
 def main():
-  global out
   pygame.init()
   screen = pygame.display.set_mode((400, 300))
   clock = pygame.time.Clock()
   font = pygame.freetype.SysFont(None, 34)
-  out = ''
   font.origin = True
   stopwatch_running = False
   while True:
@@ -41,7 +36,14 @@ def main():
         font.origin = False
         return font.origin
       if e.type == KEYDOWN and e.key == K_2:
-        renderClock(screen)
+        stopwatch_running = True
+        start()
+
+      # Inside the main game loop
+      if stopwatch_running:
+        font.render_to(screen, (100, 100), out, pygame.Color('dodgerblue'))
+        pygame.display.flip()
+        clock.tick(60)
 
 main()
 result()
