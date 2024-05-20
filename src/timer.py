f35727b1518b697d6b2fd219c384a12ea21c21ad
Chloe-Zou <142557462+Chloe-Zou@users.noreply.github.com>
2024-05-19 19:43:20 -0600

    Update timer.py

--- src/timer.py
+++ src/timer.py
@@ -1,49 +1,38 @@
 import pygame
-import pygame.freetype
-from pygame.locals import K_1, KEYDOWN, K_2
+from pygame.locals import KEYDOWN, K_l
+import time
 
-global out
+bowl = pygame.image.load('images/bowl.png')
 
-def start():
-  global out
-  start_time = 0
-  ticks = pygame.time.get_ticks() - start_time
-  millis = ticks % 1000
-  seconds = int(ticks / 1000 % 60)
-  minutes = int(ticks / 60000 % 24)
-  out = '{minutes:02d}:{seconds:02d}:{millis}'.format(minutes=minutes, millis=millis, seconds=seconds)
+def timey():
+    pygame.init()
+    window = pygame.display.set_mode((400, 300))
+    clock = pygame.time.Clock()
+    font = pygame.font.SysFont(None, 100)
+    counter = 10
+    text = font.render(str(counter), True, ('#ADD8E6'))
 
-def result():
-  pygame.init()
-  screen = pygame.display.set_mode((400, 300))
-  font = pygame.freetype.SysFont(None, 34)
-  font.origin = True
-  print(out)
-  while font.origin:
-    font.render_to(screen, (100, 100),  out, pygame.Color('dodgerblue'))
-    pygame.display.flip()
+    timer_event = pygame.USEREVENT+1
+    pygame.time.set_timer(timer_event, 1000)
 
-def main():
-  pygame.init()
-  screen = pygame.display.set_mode((400, 300))
-  clock = pygame.time.Clock()
-  font = pygame.freetype.SysFont(None, 34)
-  font.origin = True
-  stopwatch_running = False
-  while True:
-    for e in pygame.event.get():
-      if e.type == KEYDOWN and e.key == K_1:
-        font.origin = False
-        return font.origin
-      if e.type == KEYDOWN and e.key == K_2:
-        stopwatch_running = True
-        start()
-
-      # Inside the main game loop
-      if stopwatch_running:
-        font.render_to(screen, (100, 100), out, pygame.Color('dodgerblue'))
-        pygame.display.flip()
+    run = True
+    while run:
         clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            elif event.type == timer_event:
+                counter -= 1
+                text = font.render(str(counter), True, ('#ADD8E6'))
+                if counter == 0:
+                    if event.type == KEYDOWN and event.key == K_l:
+                        time.sleep(0.3)
+                        window.blit(bowl, (50, 40))
+                        pygame.display.update()
+                        print("Onion: Good job! You have finished cooking the onion stew")
+                        pygame.time.set_timer(timer_event, 0)
 
-main()
-result()
+        window.fill(('#000000'))
+        text_rect = text.get_rect(center = window.get_rect().center)
+        window.blit(text, text_rect)
+        pygame.display.flip()
