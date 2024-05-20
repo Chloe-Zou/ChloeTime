e7c49c27bfa50a6348be297ff2af6a67733473ab
Chloe-Zou <142557462+Chloe-Zou@users.noreply.github.com>
2024-05-19 19:44:26 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,10 @@
 # Importing necessary libraries and setting up the initial environment
 import pygame, sys
-from pygame.locals import QUIT, KEYDOWN, K_1
+import time
+from pygame.locals import QUIT, KEYDOWN, K_1, K_2
 from intro import importPhoto
-from character import choose_character
-
-# global chara
-# global level
-# global speed
-# global power
-# global health
-# global stamina
+from character import choose_character, chara_stats
+from recipe import recipe
 
 #Set up pygame
 pygame.init()
@@ -19,13 +14,17 @@ screen = pygame.display.set_mode((400, 300))
 # #load in images under a variable
 start_image = pygame.image.load('images/start.png')
 thunk = pygame.image.load('images/thunk.png')  
-x=1
+x = 0
 
 print("PRESS 1 TO BEGIN")
+print("PRESS 2 TO SKIP THE INTRO")
 
 # def win():
+#     if charac.health > 0:
+#         print("YOU WIN!")
 # def loss():
-#cookOff():
+def cookOff():
+    recipe(x)
 
 # Setting the initial image to start_image
 current_image = start_image
@@ -37,11 +36,24 @@ while True:
             pygame.quit()
             sys.exit()
         elif event.type == KEYDOWN:
-          if event.key == K_1 and x ==1:
-            importPhoto()
-            choose_character(screen)
-            x += 1
-            current_image = thunk
+            if event.key == K_1:
+                importPhoto()
+                choose_character(screen)
+                chara_stats()
+                time.sleep(2)
+                print(" ")
+                cookOff()
+                time.sleep(9)
+                current_image = thunk
+                sys.exit()
+            if event.key == K_2:
+                screen.blit(thunk, (100,0))
+                choose_character(screen)
+                chara_stats()
+                time.sleep(2)
+                print(" ")
+                cookOff()
+                sys.exit()
 
 
     screen.blit(current_image, (100, 0))
