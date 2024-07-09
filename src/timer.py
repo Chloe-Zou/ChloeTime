2246c961711dde022ad9d7281ef6837ce3c914a3
Chloe-Zou <142557462+Chloe-Zou@users.noreply.github.com>
2024-07-09 12:01:07 -0600

    Update timer.py

--- src/timer.py
+++ src/timer.py
@@ -2,9 +2,13 @@ import pygame
 from pygame.locals import KEYDOWN, K_l
 import time
 
+global counter
 bowl = pygame.image.load('images/bowl.png')
+def scale_image(image, factor):
+  return pygame.transform.scale(image, (image.get_width() * factor, image.get_height() * factor))
+bowl = scale_image(bowl, 8)
 
-def timey():
+def timey(screen):
     pygame.init()
     window = pygame.display.set_mode((400, 300))
     clock = pygame.time.Clock()
@@ -25,12 +29,15 @@ def timey():
                 counter -= 1
                 text = font.render(str(counter), True, ('#ADD8E6'))
                 if counter == 0:
-                    if event.type == KEYDOWN and event.key == K_l:
-                        time.sleep(0.3)
-                        window.blit(bowl, (50, 40))
-                        pygame.display.update()
-                        print("Onion: Good job! You have finished cooking the onion stew")
-                        pygame.time.set_timer(timer_event, 0)
+                    pygame.time.set_timer(timer_event, 0)
+            if event.type == KEYDOWN and event.key == K_l and counter == 0:
+                time.sleep(0.3)
+                screen.blit(bowl, (90, 70))
+                pygame.display.update()
+                print("Onion: Good job! You have finished cooking the onion stew")
+                run = False
+                return run
+                
 
         window.fill(('#000000'))
         text_rect = text.get_rect(center = window.get_rect().center)
